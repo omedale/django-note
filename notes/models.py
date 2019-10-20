672220f5dd65d58aa18ec4bbe24cb45131e29b2c
omedale <omedale@gmail.com>
2019-10-20 10:11:49 +0100

    add Note actions

--- notes/models.py
+++ notes/models.py
@@ -1,13 +1,9 @@
 from django.db import models
-from pygments.styles import get_all_styles
-
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 class Note(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   title = models.CharField(max_length=100)
   body = models.TextField()
-  style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
   class Meta:
     ordering = ['created']
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
+class NoteList(generics.ListCreateAPIView):
+  queryset = Note.objects.all()
+  serializer_class = NoteSerializer
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Note.objects.all()
+  serializer_class = NoteSerializer
