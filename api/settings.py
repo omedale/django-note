e1bbc7d1e3cbd1a741993d2bfb54d8ebaad1080d
omedale <omedale@gmail.com>
2019-11-03 13:18:14 +0100

    remove db dependences

--- api/settings.py
+++ api/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -130,4 +129,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
